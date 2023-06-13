b73056d014e1c9346dfc61242aea65c0733bdbab
Nonaka <c0b221183f@edu.teu.ac.jp>
2023-06-13 17:00:14 +0900

    問2

--- pygame_image.py
+++ pygame_image.py
@@ -7,15 +7,44 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img_2 = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img_2 = pg.transform.flip(bg_img_2, True, False)
     kokaton = pg.image.load("ex01/fig/3.png")
     kokaton = pg.transform.flip(kokaton, True, False)
+    angle = 0
+    angle_mode = 0
+    bg_x = 0
+    bg_x_2 = 1600
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [-(tmr % 1600), 0])
-        screen.blit(kokaton, [300, 200])
+
+        #display bg
+        bg_x -= 1
+        bg_x_2 -= 1
+        screen.blit(bg_img, [bg_x, 0])
+        screen.blit(bg_img_2, [bg_x_2, 0])
+
+        if bg_x <= -1600 :
+            bg_x = 1600
+        if bg_x_2 <= -1600:
+            bg_x_2 = 1600
+
+
+        # display kokaton
+        if angle_mode == 0:
+            angle += 0.1
+        else:
+            angle -= 0.1
+        
+        if angle >= 10:
+            angle_mode = 1
+        elif angle <= 0:
+            angle_mode = 0
+        kokaton_display = pg.transform.rotozoom(kokaton, angle, 1.0)
+        screen.blit(kokaton_display, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(100)
