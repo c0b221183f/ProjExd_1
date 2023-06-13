52c3d7a667568726a18b93bd3c4becb27a447719
Nonaka <c0b221183f@edu.teu.ac.jp>
2023-06-13 16:45:10 +0900

    問2

--- pygame_image.py
+++ pygame_image.py
@@ -9,25 +9,13 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     kokaton = pg.image.load("ex01/fig/3.png")
     kokaton = pg.transform.flip(kokaton, True, False)
-    angle = 0
-    angle_mode = 0
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [-(tmr % 1600), 0])
-        if angle_mode == 0:
-            angle += 0.1
-        else:
-            angle -= 0.1
-        
-        if angle >= 10:
-            angle_mode = 1
-        elif angle <= 0:
-            angle_mode = 0
-        kokaton_display = pg.transform.rotozoom(kokaton, angle, 1.0)
-        screen.blit(kokaton_display, [300, 200])
+        screen.blit(kokaton, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(100)
